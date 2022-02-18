9fa40b1a30e443ef4fa8b05de2e92389d56c8d0b
MAYKOLLofyt <97548071+MAYKOLLofyt@users.noreply.github.com>
2022-02-18 18:37:33 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -49,6 +49,7 @@ import Achievements;
 import StageData;
 import FunkinLua;
 import DialogueBoxPsych;
+import LoadingState;
 
 #if sys
 import Sys;
@@ -69,16 +70,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
+		['Practica para ser mejor', 0.2], //From 0% to 19%
+		['Mierda', 0.4], //From 20% to 39%
+		['Malisimo F', 0.5], //From 40% to 49%
 		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Meme memememe >:(', 0.69], //From 60% to 68%
+		['Muy nice', 0.7], //69%
+		['Buenisimo', 0.8], //From 70% to 79%
+		['Buen trabajo', 0.9], //From 80% to 89%
+		['Enfermo!', 1], //From 90% to 99%
+		['Perfecto!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	
 	#if (haxe >= "4.0.0")
@@ -900,7 +901,19 @@ class PlayState extends MusicBeatState
 		iconP2.visible = !ClientPrefs.hideHud;
 		add(iconP2);
 		reloadHealthBarColors();
+ var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by XMaykollYoutubeOF "), 24);
 
+                creditTxt.scrollFactor.set();
+
+                creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+
+                creditTxt.borderColor = FlxColor.BLACK;
+
+                creditTxt.borderSize = 3;
+
+                creditTxt.borderStyle = FlxTextBorderStyle.OUTLINE;
+
+                add(creditTxt);
 		scoreTxt = new FlxText(0, healthBarBG.y + 36, FlxG.width, "", 20);
 		scoreTxt.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		scoreTxt.scrollFactor.set();
@@ -908,7 +921,7 @@ class PlayState extends MusicBeatState
 		scoreTxt.visible = !ClientPrefs.hideHud;
 		add(scoreTxt);
 
-		botplayTxt = new FlxText(400, timeBarBG.y + 55, FlxG.width - 800, "BOTPLAY", 32);
+		botplayTxt = new FlxText(400, timeBarBG.y + 55, FlxG.width - 800, "Bot", 32);
 		botplayTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		botplayTxt.scrollFactor.set();
 		botplayTxt.borderSize = 1.25;
